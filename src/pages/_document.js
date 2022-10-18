6d5a5287b8436f78d4632780239f4c6ac745357f
Em Dinh <ace53thntu@gmail.com>
2022-10-18 15:42:30 +0700

    update type

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,9 +34,9 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{
               __html: `window.AicactusSDK||(window.AicactusSDK={}),AicactusSDK.load=function(a){var b=document.createElement("script");b.async=!0,b.type="text/javascript",b.src="${
                 process.env.NODE_ENV === "development"
-                  ? "http://localhost:9081/aicactus-sdk.development.min.js"
+                  ? "https://localhost:9081/aicactus-sdk.development.min.js"
                   : "https://cdn.aicactus.io/aicactus-sdk.min.js"
-              }",b.addEventListener?b.addEventListener("load",function(b){"function"==typeof a&&a(b)},!1):b.onreadystatechange=function(){("complete"==this.readyState||"loaded"==this.readyState)&&a(window.event)};let c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)},AicactusSDK.load(function(){AicactusSDK.initialize({containerId:"b8a3ccf2-5d49-4912-b2cc-87dc46e10277@web", type: "reco"}),AicactusSDK.callMethodsFromContainer()});`,
+              }",b.addEventListener?b.addEventListener("load",function(b){"function"==typeof a&&a(b)},!1):b.onreadystatechange=function(){("complete"==this.readyState||"loaded"==this.readyState)&&a(window.event)};let c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)},AicactusSDK.load(function(){AicactusSDK.initialize({containerId:"b8a3ccf2-5d49-4912-b2cc-87dc46e10277@web", type: ["reco"]}),AicactusSDK.callMethodsFromContainer()});`,
             }}
           ></script>
         </Head>
